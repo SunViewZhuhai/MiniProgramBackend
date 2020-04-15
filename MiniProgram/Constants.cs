b0259d4023fef182c61ff1c3b29c5d978de5d005
Wu Guohao <wguohao@hatfieldgroup.com>
2020-04-15 11:25:56 +0800

    Move appid and secret into constants

---
+++ MiniProgram/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MniProgram.Web
+{
+    public static class Constants
+    {
+        public static readonly string AppId = "wxce47f8975be60439";
+        public static readonly string AppSecret = "f1a6978ae2239cb7060a862d21c091e0";
+    }
+}
--- MiniProgram/Controllers/LoginController.cs
+++ MiniProgram/Controllers/LoginController.cs
@@ -7,6 +7,7 @@ using System.Net.Mime;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using MniProgram.Web.ViewModel;
 using Newtonsoft.Json;
 
@@ -24,12 +25,6 @@ namespace MniProgram.Web.Controllers
             var result = await GetUserInfo(code);
             return result;
         }
-
-        private static string _appid = "wxce47f8975be60439";
-        private static string _secret = "f1a6978ae2239cb7060a862d21c091e0";
-
-        //private static string _requestFormat =
-        //    "https://api.weixin.qq.com/sns/jscode2session?appid={0}&secret={1}&js_code={2}&grant_type=authorization_code";
         private static string _requestFormat =
             "sns/jscode2session?appid={0}&secret={1}&js_code={2}&grant_type=authorization_code";
 
@@ -40,14 +35,13 @@ namespace MniProgram.Web.Controllers
 
         private async Task<bool> GetUserInfo(string code)
         {
-            var url = string.Format(_requestFormat, _appid, _secret, code);
+            var url = string.Format(_requestFormat, Constants.AppId, Constants.AppSecret, code);
             var request = new HttpRequestMessage(HttpMethod.Get,
                 url);
             var client = _factory.CreateClient("wx");
             var response = await client.SendAsync(request);
             var resultString = await response.Content.ReadAsStringAsync();
             var result = JsonConvert.DeserializeObject<WxLoginResponseViewModel>(resultString);
-            Console.WriteLine("Session key:" + result.SessionKey + ", OpenId:" + result.OpenId);
             if (result.ErrCode == 0)
             {
                 return true;
